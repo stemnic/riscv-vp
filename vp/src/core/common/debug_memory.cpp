e209a0910c1385fc46d8ec11b0c95fc234c7b280
matt <49493714+U2654@users.noreply.github.com>
2021-02-21 12:53:13 +0100

    added define macro for byteswap

--- vp/src/core/common/debug_memory.cpp
+++ vp/src/core/common/debug_memory.cpp
@@ -6,8 +6,15 @@
 #include <stdlib.h>
 #include <errno.h>
 
-#include <arpa/inet.h>
+#ifdef __APPLE__
+#define bswap_16(value) ((((value) & 0xff) << 8) | ((value) >> 8))
+#define bswap_32(value) (((uint32_t)bswap_16((uint16_t)((value) & 0xffff)) << 16) | (uint32_t)bswap_16((uint16_t)((value) >> 16)))
+#define bswap_64(value) (((uint64_t)bswap_32((uint32_t)((value) & 0xffffffff)) << 32) | (uint64_t)bswap_32((uint32_t)((value) >> 32)))
+#else
 #include <byteswap.h>
+#endif
+
+#include <arpa/inet.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <sys/socket.h>
